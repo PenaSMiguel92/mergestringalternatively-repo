1f1e89262bba42de43bfa41654065602df4902bc
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 18:33:35 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+
+        System.out.println(sol.mergeAlternately("abc", "pqr"));
+        System.out.println(sol.mergeAlternately("ab", "pqrs"));
+        System.out.println(sol.mergeAlternately("abcd", "pq"));
     }
 }
