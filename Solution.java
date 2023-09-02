33219bb84ece46120c2aa4e185c07b107078ab43
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 18:40:21 -0700

    first attempt

--- Solution.java
+++ Solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public String mergeAlternately(String word1, String word2) {
-        return "";
+        int left = 0;
+        int right = 0;
+        boolean alt = false; //false - increment left, true - increment right;
+        String res = "";
+        
+        while (left < word1.length() || right < word2.length()) {
+            if (!alt && left < word1.length())
+                res += word1.charAt(left++);
+
+            if (alt && right < word2.length())
+                res += word2.charAt(right++);
+            
+            alt = !alt;
+        }
+        
+        return res;
     }
 }
